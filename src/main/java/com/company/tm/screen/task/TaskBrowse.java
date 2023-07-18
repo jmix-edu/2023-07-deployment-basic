4eb50de775827461f9d0605389957d7cdba27bb7
Gleb Gorelov <g.gorelov@haulmont.com>
2023-07-18 13:01:54 +0400

    Finished

--- src/main/java/com/company/tm/screen/task/TaskBrowse.java
+++ src/main/java/com/company/tm/screen/task/TaskBrowse.java
@@ -1,10 +1,10 @@
 package com.company.tm.screen.task;
 
 import com.company.tm.app.TaskImportService;
+import com.company.tm.entity.Task;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
-import com.company.tm.entity.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("tm_Task.browse")
@@ -15,6 +15,7 @@ public class TaskBrowse extends StandardLookup<Task> {
 
     @Autowired
     private TaskImportService taskImportService;
+
     @Autowired
     private CollectionLoader<Task> tasksDl;
 
